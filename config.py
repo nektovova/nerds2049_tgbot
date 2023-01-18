5440cabd5a79e7f36efa3060731f63f0a4be7217
nektovova <69082558+nektovova@users.noreply.github.com>
2023-01-18 14:24:31 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+TELEGRAM_BOT_TOKEN = '5536940449:AAHfH7mbGaRkEkxP6FvQ87m_ZCO7w5JVSWU'
+
+# CREATE DATABASE `channel_reposter_v2` CHARACTER SET utf8 COLLATE utf8_general_ci;
+DATABASE_NAME = 'channel_reposter_v2'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = '12345678'
+DATABASE_HOST = '0.0.0.0'
+DATABASE_PORT = 3306
+DATABASE_CHARSET = 'utf8'
+
+START_TEXT = '''
+Что бы добавить канал в свой список отслеживания, перешли в бота любой его пост
+
+/start - перезапуск
+/list - управление списком отслеживания
+'''
